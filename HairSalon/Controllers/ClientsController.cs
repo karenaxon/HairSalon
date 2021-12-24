6d6af32a8ce4720ef17923d2464db52c89621ae9
Kaxon <karenaxon@users.noreply.github.com>
2021-12-24 15:41:01 -0800

    Add clients home page.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -15,6 +15,11 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }  
-
+    
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client =>client.Employee).ToList();
+      return View(model);
+    }
   }
 }
